168dac4f0f9530ac3288d64188c8c276ce5436ea
Anant Narayan <anantnrg@gmail.com>
2023-11-03 15:02:34 +0530

    :sparkles: Added render method

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -87,6 +87,32 @@ impl Application {
 	}
 
 	pub fn render(&mut self) -> anyhow::Result<(), wgpu::SurfaceError> {
-		todo!()
+		let output = self.surface.get_current_texture()?;
+
+		let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+		let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+			label: Some("Render Encoder"),
+		});
+
+		{
+			let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+				label: Some("Render Pass"),
+				color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+					view: &view,
+					resolve_target: None,
+					ops: wgpu::Operations {
+						load: wgpu::LoadOp::Clear(wgpu::Color { r: 0.1, g: 0.2, b: 0.3, a: 1.0 }),
+						store: true,
+					},
+				})],
+				depth_stencil_attachment: None,
+			});
+		}
+
+		self.queue.submit(std::iter::once(encoder.finish()));
+		output.present();
+
+		Ok(())
 	}
 }
