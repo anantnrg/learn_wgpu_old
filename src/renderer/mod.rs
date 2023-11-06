aeb3608a4bda39fc96659198ebc9009a4ef7918e
Anant Narayan <anantnrg@gmail.com>
2023-11-06 22:19:44 +0530

    playing around with the camera

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -92,11 +92,11 @@ impl State {
 		let shader = device.create_shader_module(wgpu::include_wgsl!("shaders.wgsl"));
 
 		let viewport = Viewport {
-			eye: (0.0, 1.0, 2.0).into(),
+			eye: (0.0, 0.0, 1.0).into(),
 			target: (0.0, 0.0, 0.0).into(),
 			up: cgmath::Vector3::unit_y(),
 			aspect: config.width as f32 / config.height as f32,
-			fovy: 45.0,
+			fovy: 25.0,
 			znear: 0.1,
 			zfar: 100.0,
 		};
